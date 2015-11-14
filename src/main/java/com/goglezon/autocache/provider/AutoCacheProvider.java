013f98d060f2bbbcc85ac2adcc2cbe7c52abece3
SomewhereYu <yuwenqi@gmail.com>
2015-11-15 02:34:41 +0800

    啊啊

--- src/main/java/com/goglezon/autocache/provider/AutoCacheProvider.java
+++ src/main/java/com/goglezon/autocache/provider/AutoCacheProvider.java
@@ -3,6 +3,7 @@ package com.goglezon.autocache.provider;
 /**
  * Created by yuwenqi@jd.com on 2015/11/14 1:39.
  */
+
 public interface AutoCacheProvider {
     Object get(String key);
     void set(String key,Object value,int expiredTimeSec);
